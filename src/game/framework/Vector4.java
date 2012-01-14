00f4c79dbc4c60f271a1447ebd4761db5e62791a
Annuate <annuate@gmail.com>
2012-01-14 16:28:57 -0500

    Added toString, one, unitX, unitY, unitZ, and unitW methods to Vector4. Added stubs for add, addX, addY, addZ, addW, barycentric, catmullRom, clamp, distance, distanceSquared, divide, dotProduct, equals, hermite, magnitude, magnitudeSquared, lerp, max, min, multiply, negate, normalize, smoothStep, subtract, subtractX, subtractY, subtractZ, subtractW, and transform to the Vector4 class.

--- src/game/framework/Vector4.java
+++ src/game/framework/Vector4.java
@@ -9,7 +9,6 @@ public class Vector4 extends Vector3
 {
     /**
      * The w component of the Vector4.
-     * Also referred to as k component in vectorial notation. 
      */
     public final float w;
     
@@ -119,5 +118,215 @@ public class Vector4 extends Vector3
         this(vec2.x, vec2.y, z, w);
     }
     //</editor-fold>
-            
+    
+    //<editor-fold defaultstate="collapsed" desc="Add">
+    public static Vector4 add(Vector4 a, Vector4 b){return null;}
+    public static Vector4 add(Vector4 a, Vector3 b){return null;}
+    public static Vector4 add(Vector4 a, Vector2 b){return null;}
+    public Vector4 add(Vector4 that){return null;}
+    @Override
+    public Vector4 add(Vector3 that){return null;}
+    @Override
+    public Vector4 add(Vector2 that){return null;}
+    public Vector4 add(float x, float y, float z, float w){return null;}
+    public Vector4 add(int x, int y, int z, int w){return null;}
+    public Vector4 add(Vector3 vec, float w){return null;}
+    public Vector4 add(Vector2 vec, float z, float w){return null;}
+    
+    @Override
+    public Vector4 addX(float x){return null;}
+    @Override
+    public Vector4 addX(int x){return null;}
+    @Override
+    public Vector4 addY(float y){return null;}
+    @Override
+    public Vector4 addY(int y){return null;}
+    @Override
+    public Vector4 addZ(float z){return null;}
+    @Override
+    public Vector4 addZ(int z){return null;}
+    public Vector4 addW(float w){return null;}
+    public Vector4 addW(int w){return null;}
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Barycentric">
+    public static Vector4 barycentric(){return null;}
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="CatmullRom">
+    public static Vector4 catmullRom(){return null;}
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Clamp">
+    public static Vector4 clamp(Vector4 value, Vector4 min, Vector4 max){return null;}
+    public Vector4 clamp(Vector4 min, Vector4 max){return null;}
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Distance">
+    public static Vector4 distance(Vector4 a, Vector4 b){return null;}
+    public Vector4 distance(Vector4 that){return null;}
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Distance Squared">
+    public static Vector4 distanceSquared(Vector4 a, Vector4 b){return null;}
+    public Vector4 distanceSquared(Vector4 that){return null;}
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Divide">
+    public static Vector4 divide(Vector4 a, float scalar){return null;}
+    public static Vector4 divide(Vector4 a, int scalar){return null;}
+    @Override
+    public Vector4 divide(float scalar){return null;}
+    @Override
+    public Vector4 divide(int scalar){return null;}
+    public static Vector4 divide(Vector4 a, Vector4 b){return null;}
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Dot Product">
+    public static float dotProduct(Vector4 a, Vector4 b){return -1;}
+    public static float dotProduct(Vector4 a, Vector3 b){return -1;}
+    public static float dotProduct(Vector4 a, Vector2 b){return -1;}
+    public float dotProduct(Vector4 that){return -1;}
+    @Override
+    public float dotProduct(Vector3 that){return -1;}
+    @Override
+    public float dotProduct(Vector2 that){return -1;}
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Equals">
+    @Override
+    public boolean equals(Object object){return false;}
+    public static boolean equals(Vector4 a, Vector4 b){return false;}
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Hermite">
+    public static Vector4 hermite(){return null;}
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Magnitude">
+    public static float magnitude(Vector4 a){return -1;}
+    @Override
+    public float magnitude(){return -1;}
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Magnitude Squared">
+    public static float magnitudeSquared(Vector4 a){return -1;}
+    @Override
+    public float magnitudeSquared(){return -1;}
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Linear Interpolation">
+    public static Vector4 lerp(Vector4 a, Vector4 b){return null;}
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Max and Min">
+    public static Vector4 max(Vector4 a, Vector4 b){return null;}
+    
+    public static Vector4 min(Vector4 a, Vector4 b){return null;}
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Multiply">
+    public static Vector4 multiply(Vector4 a, float scalar){return null;}
+    public static Vector4 multiply(Vector4 a, int scalar){return null;}
+    @Override
+    public Vector4 multiply(float scalar){return null;}
+    @Override
+    public Vector4 multiply(int scalar){return null;}
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Negate">
+    public static Vector4 negate(Vector4 a){return null;}
+    @Override
+    public Vector4 negate(){return null;}
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Normalize">
+    public static Vector4 normalize(Vector4 a){return null;}
+    @Override
+    public Vector4 normalize(){return null;}
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Smooth Step">
+    public static Vector4 smoothStep(Vector4 a, Vector4 b){return null;}
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Special Vectors">
+    public static Vector4 one()
+    {
+        return new Vector4(1.0f);
+    }
+    
+    public static Vector4 unitX()
+    {
+        return new Vector4(1.0f, 0.0f, 0.0f, 0.0f);
+    }
+    
+    public static Vector4 unitY()
+    {
+        return new Vector4(0.0f, 1.0f, 0.0f, 0.0f);
+    }
+    
+    public static Vector4 unitZ()
+    {
+        return new Vector4(0.0f, 0.0f, 1.0f, 0.0f);
+    }
+    
+    public static Vector4 unitW()
+    {
+        return new Vector4(0.0f, 0.0f, 0.0f, 1.0f);
+    }
+    
+    public static Vector4 zero()
+    {
+        return new Vector4(0.0f);
+    }
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Subtract">
+    public static Vector4 subtract(Vector4 a, Vector4 b){return null;}
+    public static Vector4 subtract(Vector4 a, Vector3 b){return null;}
+    public static Vector4 subtract(Vector3 a, Vector4 b){return null;}
+    public static Vector4 subtract(Vector4 a, Vector2 b){return null;}
+    public static Vector4 subtract(Vector2 a, Vector4 b){return null;}
+    public Vector4 subtract(Vector4 that){return null;}
+    @Override
+    public Vector4 subtract(Vector3 that){return null;}
+    @Override
+    public Vector4 subtract(Vector2 that){return null;}
+    public Vector4 subtract(float x, float y, float z, float w){return null;}
+    public Vector4 subtract(int x, int y, int z, int w){return null;}
+    public Vector4 subtract(Vector3 vec, float w){return null;}
+    public Vector4 subtract(Vector2 vec, float z, float w){return null;}
+    
+    @Override
+    public Vector4 subtractX(float x){return null;}
+    @Override
+    public Vector4 subtractX(int x){return null;}
+    @Override
+    public Vector4 subtractY(float y){return null;}
+    @Override
+    public Vector4 subtractY(int y){return null;}
+    @Override
+    public Vector4 subtractZ(float z){return null;}
+    @Override
+    public Vector4 subtractZ(int z){return null;}
+    public Vector4 subtractW(float w){return null;}
+    public Vector4 subtractW(int w){return null;}
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="To String">
+    /**
+     * Converts the Vector4 to a string. <x,y,z,w>
+     * @return Returns a string representing the Vector4.
+     */
+    @Override
+    public String toString()
+    {
+        return "<" + this.x + "," + this.y + "," + this.z + "," + this.w + ">";
+    }
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Transformations">
+    public static Vector4 transform(Vector4 a, Vector4 b){return null;}
+    //</editor-fold>
 }

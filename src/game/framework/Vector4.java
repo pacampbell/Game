066806b265a72671f4056edc2b1e65443475e1b2
Annuate <annuate@gmail.com>
2012-01-14 14:17:14 -0500

    Added extends clause, complete constructor and some basic documentation to the Vector4 class.

--- src/game/framework/Vector4.java
+++ src/game/framework/Vector4.java
@@ -1,9 +1,29 @@
 package game.framework;
 
 /**
+ * Vector4 class representing a vector of <x, y, z, w> components.
+ * @version incomplete
  * @author paul
  */
-public class Vector4 
+public class Vector4 extends Vector3
 {
+    /**
+     * The w component of the Vector4.
+     * Also referred to as k component in vectorial notation. 
+     */
+    public final float w;
     
+    /**
+     * Complete Constructor
+     * Sets x, y, z, and w components of Vector4
+     * @param x x component of Vector4
+     * @param y y component of Vector4
+     * @param z z component of Vector4
+     * @param w w component of Vector4
+     */
+    public Vector4(float x, float y, float z, float w)
+    {
+        super(x, y, z);
+        this.w = w;
+    }
 }

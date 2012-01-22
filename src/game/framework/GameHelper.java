eacd524321fa8b7d06cbc2c5f061b81a2d2ee92d
Annuate <annuate@gmail.com>
2012-01-21 23:26:11 -0500

    Added documentation to the GameHelper class. Added a protected setScreenHeight, and setScreenWidth method to be used internally within the framework. Added public static methods WIDTH, and HEIGHT.

--- src/game/framework/GameHelper.java
+++ src/game/framework/GameHelper.java
@@ -4,6 +4,14 @@ import java.net.URL;
 
 public class GameHelper
 {
+    private static int width = 0;
+    private static int height = 0;
+    
+    /**
+     * Uses a hack to return a path to a image file based on the operating system that is being used.
+     * @param path Relative path to the file that needs a path.
+     * @return Returns a path to the file.
+     */
     public static URL osIndependentFilePath(String path)
     {
 
@@ -20,11 +28,46 @@ public class GameHelper
             StackTraceElement[] sTrace = e.getStackTrace();
             // sTrace[0] will be always there
             systemIndependentPath = sTrace[0].getClass().getResource(fullPath);
-            //System.out.println(systemIndependentPath);
 	}
         finally
         {
             return systemIndependentPath;
         }
+    }
+    
+    /**
+     * Stores the width of the Game Window.
+     * only to be used within the framework itself.
+     * @param width Width of the Game Window.
+     */
+    protected static void setScreenWidth(int width)
+    {
+        GameHelper.width = width;
+    }
+    
+    /**
+     * Stores the height of the Game Window.
+     * only to be used within the framework itself.
+     * @param width Height of the Game Window.
+     */
+    protected static void setScreenHeight(int height)
+    {
+        GameHelper.height = height;
+    }
+    
+    /**
+     * @return Returns the width of the game window. 
+     */
+    public static int WIDTH()
+    {
+        return GameHelper.width;
+    }
+    
+    /**
+     * @return Returns the height of the game window. 
+     */
+    public static int HEIGHT()
+    {
+        return GameHelper.height;
     }
 }

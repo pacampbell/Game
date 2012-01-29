258b73bfb210a75677b55ac3f32f56526cd66681
Annuate <annuate@gmail.com>
2012-01-28 22:55:09 -0500

    Adjusted the fontLoader method.

--- src/game/graphics/FontHelper.java
+++ src/game/graphics/FontHelper.java
@@ -8,16 +8,17 @@ public class FontHelper
     /**
      * Class to help loading custom fonts.
      * @param path Path to custom font.
+     * @param size Size of the font to be loaded.
      * @return Returns a font object containing the custom font.
      */
-    public static Font loadFont(String path)
+    public static Font loadFont(String path, float size)
     {
         Font font = null;
         String fName = "/resources/" + path;
         try
         {
             InputStream is = FontHelper.class.getResourceAsStream(fName);
-            font = Font.createFont(Font.TRUETYPE_FONT, is);
+            font = Font.createFont(Font.TRUETYPE_FONT, is).deriveFont(size);
         } 
         catch (Exception ex) 
         {

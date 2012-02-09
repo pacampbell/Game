00e3f5a00aceabba221b0c44115b648311a777be
Annuate <annuate@gmail.com>
2012-02-09 10:55:00 -0500

    Added Keyboard and Mouse class.

---
+++ src/game/input/Keyboard.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game.input;
+
+/**
+ *
+ * @author paul
+ */
+public class Keyboard {
+    
+}

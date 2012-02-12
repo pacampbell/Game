8975b64a2f3f2d1b2ce46b6a841a8ad7d22c5dac
Annuate <annuate@gmail.com>
2012-02-12 08:46:35 -0500

    Completed Keyboard.java

--- src/game/input/Keyboard.java
+++ src/game/input/Keyboard.java
@@ -1,14 +1,53 @@
 package game.input;
 
+import java.awt.event.KeyEvent;
+
 /**
- *
+ * Static wrapper class for the KeyboardInput object.
  * @author paul
  */
-public class Keyboard {
-    public static KeyboardInput keyboard;
+public class Keyboard 
+{
+    /**
+     * Static instance of the keyboard to be used.
+     */
+    public static KeyboardInput keyboard = new KeyboardInput();
     
-    public static void initKeyboard()
+    /**
+     * Polls the keyboard for new keys that might be pressed this update.
+     */
+    public void poll()
     {
-        keyboard = new KeyboardInput();
+        keyboard.poll();
+    }
+    
+    /**
+     * Checks to see if a key is being held down.
+     * @param keyCode ID for the key to be evaluated.
+     * @return Returns if the key is being held.
+     */
+    public static boolean keyDown(int keyCode)
+    {
+        return keyboard.keyDown(keyCode);
+    }
+    
+    /**
+     * Checks to see if a key was pressed.
+     * @param keyCode ID for the key to be evaluated.
+     * @return Return is the key was pressed. 
+     */
+    public static boolean keyDownOnce(int keyCode)
+    {
+        return keyboard.keyDownOnce(keyCode);
+    }
+    
+    public static void keyPressed(KeyEvent e)
+    {
+        keyboard.keyPressed(e);
+    }
+    
+    public static void keyReleased(KeyEvent e)
+    {
+        keyboard.keyReleased(e);
     }
 }

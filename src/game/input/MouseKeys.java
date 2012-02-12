30ccce7cf43bcc6ce9c893c63de3718af8d17173
Annuate <annuate@gmail.com>
2012-02-12 08:55:10 -0500

    Added MouseKeys enum to the project.

---
+++ src/game/input/MouseKeys.java
@@ -0,0 +1,12 @@
+package game.input;
+
+public enum MouseKeys 
+{
+    BUTTON_1(1), BUTTON_2(2), BUTTON_3(3);
+    
+    public final int ID;
+    MouseKeys(int ID)
+    {
+        this.ID = ID;
+    }
+}

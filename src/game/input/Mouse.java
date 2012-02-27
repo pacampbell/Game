c5ffb4b0fa2cf804c35facd509157f4c8fce15ec
Annuate <annuate@gmail.com>
2012-02-27 18:24:47 -0500

    Added two overloads to the Mouse static class.

--- src/game/input/Mouse.java
+++ src/game/input/Mouse.java
@@ -41,13 +41,33 @@ public class Mouse
     }
     
     /**
+     * Checks to see if a button on the mouse is being held.
+     * @param key MouseKey object containing the key code of the mouse button that is pressed.
+     * @return Returns if the button is being held.
+     */
+    public static boolean buttonDownOnce(MouseKeys key)
+    {
+        return Mouse.buttonDownOnce(key.KEYCODE);
+    }
+    
+    /**
      * Checks to see if a button on the mouse is pressed.
      * @param button ID for the button to be evaluated.
-     * @return Returns if the button was pressed.
+     * @return Returns if the button is down.
      */
     public static boolean buttonDown(int button)
     {
         return mouse.buttonDown(button);
+    }
+    
+    /**
+     * Checks to see if a button on the mouse is pressed.
+     * @param key MouseKey object containing the key code of the mouse button that is pressed.
+     * @return Returns if the button is down. 
+     */
+    public static boolean buttonDown(MouseKeys key)
+    {
+        return Mouse.buttonDown(key.KEYCODE);
     }
     
     public static void mousePressed(MouseEvent e)

1ff8ea2588afd4c078cb140cdd224efe3e9f764c
Annuate <annuate@gmail.com>
2012-03-19 17:40:21 -0400

    Added Javadoc to MouseKeys.java

--- src/game/input/MouseKeys.java
+++ src/game/input/MouseKeys.java
@@ -2,10 +2,31 @@ package game.input;
 
 public enum MouseKeys 
 {
-    BUTTON_1(1), BUTTON_2(2), BUTTON_3(3);
+    /**
+     * Button_1 represents the right mouse button.
+     */
+    BUTTON_1(1), 
     
+    /**
+     * Button_2 represents the middle mouse button.
+     */
+    BUTTON_2(2), 
+    
+    /**
+     * Button_3 represents the left mouse button.
+     */
+    BUTTON_3(3);
+    
+    /**
+     * Integer representing the key code of the mouse button.
+     */
     public final int KEYCODE;
     
+    /**
+     * Private Constructor.
+     * Creates a MouseKeys enum with the provided key code.
+     * @param keycode Integer representing the key code of a mouse button.
+     */
     private MouseKeys(int keycode)
     {
         this.KEYCODE = keycode;

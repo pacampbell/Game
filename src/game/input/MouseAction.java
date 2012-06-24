83b0fba715f8141829a4e381b5f8440d1fcb5791
Annuate <annuate@gmail.com>
2012-06-24 10:59:35 -0400

    Added a MouseAction enum to the prject. This class represents different mouse events that could occur.

---
+++ src/game/input/MouseAction.java
@@ -0,0 +1,16 @@
+package game.input;
+
+/**
+ * Enumeration representing the different Events/Actions a mouse can perform.
+ * @author paul
+ */
+public enum MouseAction 
+{
+    CLICK,
+    DOUBLE_CLICK,
+    MOUSE_DOWN,
+    MOUSE_MOVE,
+    MOUSE_OUT,
+    MOUSE_OVER,
+    MOUSE_UP
+}

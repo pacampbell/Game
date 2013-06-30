1a51762e84675ddfb799487121661888a994d26c
Paul Campbell <paul.campbell@stonybrook.edu>
2013-06-30 15:14:11 -0400

    Added new directory to contain unit tests. Created a stubbed unit test for Vector2.

---
+++ src/test/framework/Vector2Test.java
@@ -0,0 +1,204 @@
+package test.framework;
+
+import org.junit.Test;
+
+/**
+ * Unit test for Vector2.
+ */
+public class Vector2Test {
+    @Test
+    public void testAdd() throws Exception {
+
+    }
+
+    @Test
+    public void testAddX() throws Exception {
+
+    }
+
+    @Test
+    public void testAddY() throws Exception {
+
+    }
+
+    @Test
+    public void testBarycentric() throws Exception {
+
+    }
+
+    @Test
+    public void testCatmullRom() throws Exception {
+
+    }
+
+    @Test
+    public void testClamp() throws Exception {
+
+    }
+
+    @Test
+    public void testDirectionAlpha() throws Exception {
+
+    }
+
+    @Test
+    public void testDirectionBeta() throws Exception {
+
+    }
+
+    @Test
+    public void testDistance() throws Exception {
+
+    }
+
+    @Test
+    public void testDistanceSquared() throws Exception {
+
+    }
+
+    @Test
+    public void testDivide() throws Exception {
+
+    }
+
+    @Test
+    public void testDotProduct() throws Exception {
+
+    }
+
+    @Test
+    public void testEquals() throws Exception {
+
+    }
+
+    @Test
+    public void testHermite() throws Exception {
+
+    }
+
+    @Test
+    public void testLerp() throws Exception {
+
+    }
+
+    @Test
+    public void testMagnitude() throws Exception {
+
+    }
+
+    @Test
+    public void testMagnitudeSquared() throws Exception {
+
+    }
+
+    @Test
+    public void testMax() throws Exception {
+
+    }
+
+    @Test
+    public void testMin() throws Exception {
+
+    }
+
+    @Test
+    public void testMultiply() throws Exception {
+
+    }
+
+    @Test
+    public void testNegate() throws Exception {
+
+    }
+
+    @Test
+    public void testNormalize() throws Exception {
+
+    }
+
+    @Test
+    public void testPerpendicular() throws Exception {
+
+    }
+
+    @Test
+    public void testScalarProjection() throws Exception {
+
+    }
+
+    @Test
+    public void testVectorProjection() throws Exception {
+
+    }
+
+    /*
+    TODO: Fix the implementation of random
+    @Test
+    public void testRandom() throws Exception {
+
+    }
+    */
+
+    @Test
+    public void testReflect() throws Exception {
+
+    }
+
+    @Test
+    public void testSmoothStep() throws Exception {
+
+    }
+
+    @Test
+    public void testOne() throws Exception {
+
+    }
+
+    @Test
+    public void testUnitX() throws Exception {
+
+    }
+
+    @Test
+    public void testUnitY() throws Exception {
+
+    }
+
+    @Test
+    public void testZero() throws Exception {
+
+    }
+
+    @Test
+    public void testSubtract() throws Exception {
+
+    }
+
+    @Test
+    public void testSubtractX() throws Exception {
+
+    }
+
+    @Test
+    public void testSubtractY() throws Exception {
+
+    }
+
+    @Test
+    public void testToString() throws Exception {
+
+    }
+
+    /*
+    TODO: Need to implement transform before being able to test it :)
+    @Test
+    public void testTransform() throws Exception {
+
+    }
+    */
+
+    @Test
+    public void testWedgeProduct() throws Exception {
+
+    }
+}
